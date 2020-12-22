37e013b056593c4518a55c3f3f5c4144fcbd2645
Matheus <matheussouzaslv2@gmail.com>
2020-12-22 12:42:27 -0300

    Finalizando Projeto - Mais Correções

--- src/modules/fruits/fruits.controller.ts
+++ src/modules/fruits/fruits.controller.ts
@@ -1,9 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put  } from '@nestjs/common';
+import { Fruits } from 'src/database/entities/Fruits.entity';
+import { CreateFruitsDto } from './dtos/create-fruits.dtos';
+import { UpdateFruitsDto } from './dtos/update-fruits.dto';
+import { FruitsService } from './fruits.service';
 
 @Controller('fruits')
 export class FruitsController {
+    constructor(
+        private readonly fruitsService: FruitsService
+    ) { }
+
     @Get()
-    findAll(): string {
-        return "Listando Frutas";
+    findAll(): Promise<Fruits[]> {
+        return this.fruitsService.findAll();
+    }
+
+    @Get(':id')
+    findById(@Param('id') id: string): Promise<Fruits> {
+        return this.fruitsService.findById(id);
+    }
+
+    @Post()
+    create(@Body() dto: CreateFruitsDto) {
+        this.fruitsService.create(dto);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() dto: UpdateFruitsDto) {
+        this.fruitsService.update(id, dto);
+    }
+
+    @Delete(':id') 
+    delete(@Param('id') id: string) {
+        this.fruitsService.delete(id);
     }
 }
--- src/modules/fruits/fruits.service.ts
+++ src/modules/fruits/fruits.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Fruits } from 'src/database/entities/Fruits.entity';
 import { Repository } from 'typeorm';
+import { CreateFruitsDto } from './dtos/create-fruits.dtos';
+import { UpdateFruitsDto } from './dtos/update-fruits.dto';
 
 @Injectable()
 export class FruitsService {
     constructor(
-        @Injectable(Fruits)
+        @InjectRepository(Fruits)
         private fruitsRepository: Repository<Fruits>
     ) { }
 
@@ -13,5 +16,19 @@ export class FruitsService {
             return this.fruitsRepository.find();
         }
 
+        findById(id: string): Promise<Fruits> {
+            return this.fruitsRepository.findOne(id);
+        }
 
+        create(dto: CreateFruitsDto){
+            this.fruitsRepository.insert({Name: dto.name, PictureUrl: dto.PictureUrl});
+        }
+
+        update(id: string, dto: UpdateFruitsDto) {
+            this.fruitsRepository.update(id, { Name: dto.name, PictureUrl: dto.PictureUrl });
+        }
+    
+        delete(id: string) {
+            this.fruitsRepository.delete(id);
+        }
 }
