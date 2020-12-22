e5e3c3c0bbf5cfed176af8b15f6b06aae23cda39
Matheus <matheussouzaslv2@gmail.com>
2020-12-22 11:46:19 -0300

    Controller e Module Criadas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FruitsModule } from './modules/fruits/fruits.module';
 
 @Module({
-  imports: [],
+  imports: [FruitsModule],
   controllers: [AppController],
   providers: [AppService],
 })
